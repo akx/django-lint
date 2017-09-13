557653e0f15c265d991ff550ddd508aa9a935afa
Aarni Koskela <akx@iki.fi>
2017-09-13 16:17:24 +0300

    Refactor readme generator

--- update_readme_checkers.py
+++ update_readme_checkers.py
@@ -6,9 +6,6 @@ import re
 from django_lint.checks.registry import get_check_classes
 from io import StringIO
 
-start_re = re.compile('^## Checkers\n', re.MULTILINE)
-end_re = re.compile('^## ', re.MULTILINE)
-
 
 def generate_readme_fragment():
     out_io = StringIO()
@@ -17,14 +14,30 @@ def generate_readme_fragment():
         out_io.write('{description}\n'.format(description=dedent(check_class.description)))
     return out_io.getvalue()
 
-readme_fragment = generate_readme_fragment()
-with open('README.md', 'r') as infp:
-    old_readme = infp.read()
-start = start_re.search(old_readme)
-replace_from = start.span()[1]
-end = end_re.search(old_readme, start.span()[1])
-replace_to = end.span()[0]
-new_readme = old_readme[:replace_from] + '\n' + readme_fragment + old_readme[replace_to:]
 
-with open('README.md', 'w') as outfp:
-    outfp.write(new_readme)
+def replace_readme_segment(old_readme, readme_fragment, start_re, end_re):
+    start = start_re.search(old_readme)
+    replace_from = start.span()[1]
+    end = end_re.search(old_readme, start.span()[1])
+    replace_to = end.span()[0]
+    new_readme = old_readme[:replace_from] + '\n' + readme_fragment + old_readme[replace_to:]
+    return new_readme
+
+
+def main():
+    start_re = re.compile('^## Checkers\n', re.MULTILINE)
+    end_re = re.compile('^## ', re.MULTILINE)
+
+    readme_fragment = generate_readme_fragment()
+
+    with open('README.md', 'r') as infp:
+        old_readme = infp.read()
+
+    new_readme = replace_readme_segment(old_readme, readme_fragment, start_re, end_re)
+
+    with open('README.md', 'w') as outfp:
+        outfp.write(new_readme)
+
+
+if __name__ == '__main__':
+    main()
