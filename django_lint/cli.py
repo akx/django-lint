43352df736d7ff4b9d3c983e6f50775a6d8b98b1
Aarni Koskela <akx@iki.fi>
2019-01-18 17:12:57 +0200

    Refactor cli a bit

--- django_lint/cli.py
+++ django_lint/cli.py
@@ -1,8 +1,8 @@
 import argparse
-
 import sys
-
 import os
+
+import astroid
 
 from django_lint.checks.registry import get_check_classes
 from django_lint.contexts import Context, FileContext
@@ -12,11 +12,31 @@ parser = argparse.ArgumentParser()
 parser.add_argument('target', nargs='*')
 parser.add_argument('--list-checks', '-l', action='store_true', help='list checks, then exit')
 parser.add_argument('--disable', '-d', action='append', help='disable these checks')
+parser.add_argument('--only', action='append', help='enable only these checks')
 parser.add_argument('--fast', action='store_true', help='tell checks to try and be faster (though less accurate)')
 parser.add_argument(
     '--prepend-to-path', '-p', action='append',
     help='prepend this to `sys.path` – allows analysis to find libraries that might be in other venvs'
 )
+
+
+def check_file(context, filename, check_classes):
+    file_context = FileContext(context, filename)
+    for check_cls in check_classes:
+        if file_context.is_migration and not check_cls.run_on_migrations:
+            continue
+
+        errors = check_cls(file_context).check()
+        if errors is None:
+            continue
+        for error in errors:
+            print(error)
+
+
+def is_check_enabled(cls, only, disable):
+    if only:
+        return (cls.id in only)
+    return (cls.id not in (disable or ()))
 
 
 def cli(args=None):
@@ -32,16 +52,10 @@ def cli(args=None):
 
     files = set(f for f in collect_files(targets=args.target) if f.endswith('.py'))
     context = Context(files=sorted(files), fast=args.fast)
-    check_classes = {c for c in get_check_classes() if c.id not in (args.disable or ())}
+    check_classes = {c for c in get_check_classes() if is_check_enabled(c, only=args.only, disable=args.disable)}
 
     for filename in context.files:
-        file_context = FileContext(context, filename)
-        for check_cls in check_classes:
-            if file_context.is_migration and not check_cls.run_on_migrations:
-                continue
-
-            errors = check_cls(file_context).check()
-            if errors is None:
-                continue
-            for error in errors:
-                print(error)
+        try:
+            check_file(context, filename, check_classes)
+        except astroid.exceptions.AstroidSyntaxError as ase:
+            print('Unable to parse %s: %s' % (filename, ase), file=sys.stderr)
--- django_lint/files.py
+++ django_lint/files.py
@@ -1,9 +1,20 @@
 import os
 
 SKIPPED_DIRS = {
+    '.awcache',
+    '.cache',
     '.git',
+    '.idea',
     '__pycache__',
     'node_modules',
+    '.mypy_cache',
+}
+
+SKIPPED_PATH_SEGMENTS = {
+    'var/media',
+    'var/static',
+    '.tox',
+    'site-packages',
 }
 
 
@@ -11,7 +22,15 @@ def collect_files(targets):
     for target in targets:
         if os.path.isdir(target):
             for dirpath, dirnames, filenames in os.walk(target):
-                dirnames[:] = [dirname for dirname in dirnames if dirname not in SKIPPED_DIRS]
+                dirnames[:] = [
+                    dirname
+                    for dirname
+                    in dirnames
+                    if not (
+                        dirname in SKIPPED_DIRS or
+                        any(seg in os.path.join(dirpath, dirname) for seg in SKIPPED_PATH_SEGMENTS)
+                    )
+                ]
                 yield from (os.path.realpath(os.path.join(dirpath, filename)) for filename in filenames)
         else:
             yield os.path.realpath(target)
